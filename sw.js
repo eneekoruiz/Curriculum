7f3b55fda9944dfefa0d476d8861e3dd4e90e5d5
eneekoruiz <eruiz084@ikasle.ehu.eus>
2026-05-06 15:36:02 +0200

    feat: upgrade to PWA with offline support and international ARIA accessibility

--- sw.js
+++ sw.js
@@ -1,23 +1,19 @@
-// Simple Service Worker to enable PWA installability
 const CACHE_NAME = 'eneko-cv-v1';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/favicon.png',
-  '/og.png',
-  '/manifest.json'
+  './',
+  './index.html',
+  './translations.js',
+  './favicon.png'
 ];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
+self.addEventListener('install', (e) => {
+  e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
